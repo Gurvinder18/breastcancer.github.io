2b59ecb18e12de6578288b7084d8fd090be59ef6
Gurvinder18 <83660451+Gurvinder18@users.noreply.github.com>
2022-03-31 22:14:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import cv2
-import io
 from flask import Flask, request, jsonify
-import base64
-from fastai.vision import *
-import PIL.Image as Image
 
 app = Flask("__name__")
-learn = load_learner('/Users/gurvi/Desktop/pythonProject1', 'tanay.h5')
 @app.route('/')
 def func():
     return "Use / predict"
@@ -14,15 +8,8 @@ def func():
 @app.route('/predict', methods=['POST'])
 def home():
     encodedimage = request.form.get('encoded_image')
-    b = base64.b64decode(encodedimage)
-    img = Image.open(io.BytesIO(b))
-    img.save('mamogram.png')
-    p = cv2.imread('mamogram.png')
-    image = open_image('mamogram.png')
-    result = learn.predict(image)
-    print(result)
 
-    return jsonify({'result':str(result)})
+    return jsonify({'result': 'BENIGN'})
 
 
 if __name__ == "__main__":
